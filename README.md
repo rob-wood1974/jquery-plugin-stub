65dfe54a55cc1b56177e6033df2fa9b051c38137
Rob Wood <flatulentdog@hotmail.com>
2016-01-25 19:18:58 -0500

    Base for grunt-init jquery plugin

---
+++ README.md
@@ -0,0 +1,28 @@
+# grunt-init-jquery-plugin-stub
+> Create a jQuery plugin with [grunt-init][], including unit tests and other operations.
+
+[grunt-init]: http://gruntjs.com/project-scaffolding
+
+## Installation
+If you haven't already done so, install [grunt-init][].
+
+Once grunt-init is installed, place this template in your `~/.grunt-init/` directory. It's recommended that you use git to clone this template into that directory, as follows:
+
+```
+git clone https://github.com/gruntjs/grunt-init-jquery-plugin-stub.git ~/.grunt-init/jquery-plugin-stub
+```
+
+```
+_Windows users:
+git clone https://github.com/gruntjs/grunt-init-jquery-plugin-stub.git %userprofile%/.grunt-init/jquery-plugin-stub
+```
+
+## Usage
+
+At the command-line, cd into an empty directory, run this command and follow the prompts.
+
+```
+grunt-init jquery-plugin-stub
+```
+
+_Note that this template will generate files in the current directory, so be sure to change to a new directory first if you don't want to overwrite existing files._
---
+++ root/src/name.js
@@ -0,0 +1,154 @@
+/*!
+ * {%= name %}
+ * {%= homepage %}
+ * (jQuery Mobile) jQuery UI Widget Factory + RequireJS plugin/module boilerplate (for 1.8/9+)
+ * Author: @scottjehl, @jrburke, @addyosmani
+ * Further changes: @addyosmani
+ * Further Further changes: @rob-wood1974
+ * Copyright (c) {%= grunt.template.today('yyyy') %} {%= author_name %}
+ * Licensed under the {%= licenses.join(', ') %} license{%= licenses.length === 1 ? '' : 's' %}.
+ */
+
+ /*██████████████████████████████████████████████████████████
+ █ This is a combination of jQuery UI Widget + RequireJS module boilerplate (for 1.8/9+)
+ █ and (jQuery mobile) jQuery UI Widget-factory plugin boilerplate (for 1.8/9+),
+ █ from https://github.com/jquery-boilerplate/jquery-patterns
+ █ RW 2016
+███████████████████████████████████████████████████████████*/
+ 
+// Note from James:
+//
+// This assumes you are using the RequireJS+jQuery file, and
+// that the following files are all in the same directory:
+//
+// - require-jquery.js
+// - jquery-ui.custom.min.js (custom jQuery UI build with widget factory)
+// - templates/
+//    - asset.html
+// - ao.myWidget.js
+
+// Then you can construct the widget like so:
+
+// {%= js_safe_name %}.js file:
+// Uncomment this version for a sample using templates
+// define(["jquery", "text!templates/asset.html", "jquery-ui.custom.min","jquery.tmpl"], function ($, assetHtml) {
+define(["jquery", "jqueryui"], function ($, window, document, undefined) {
+
+    // define your widget under a namespace of your choice
+    // 'ao' is used here as a demonstration
+    $.widget( "{%= js_safe_name %}", {
+
+        // Options to be used as defaults
+        options: {
+            foo: true,
+            bar: false
+        },
+
+        // Set up widget (e.g. create element, apply theming,
+        // bind events, etc.)
+        _create: function () {
+
+            // _create will automatically run the first time
+            // this widget is called. Put the initial widget
+            // set-up code here, then you can access the element
+            // on which the widget was called via this.element.
+            // The options defined above can be accessed via
+            // this.options
+
+            //this.element.addStuff();
+            //this.element.addStuff();
+            //this.element.tmpl(assetHtml).appendTo(this.content);
+        },
+
+        // Destroy an instantiated plugin and clean up modifications
+        // that the widget has made to the DOM
+        destroy: function () {
+            //t his.element.removeStuff();
+            // For UI 1.8, destroy must be invoked from the base
+            // widget
+            $.Widget.prototype.destroy.call( this );
+            // For UI 1.9, define _destroy instead and don't worry
+            // about calling the base widget
+        },
+
+        // Private methods/props start with underscores
+        _dosomething: function(){  },
+
+        // Public methods like these below can can be called
+                // externally:
+        // $("#myelem").foo( "enable", arguments );
+
+        enable: function() {  },
+
+        methodB: function ( event ) {
+            // _trigger dispatches callbacks the plugin user can
+            // subscribe to
+            //signature: _trigger( "callbackName" , [eventObject],
+            // [uiObject] )
+            console.log("methodB called");
+        },
+
+        methodA: function ( event ) {
+            this._trigger("dataChanged", event, {
+                key: "someValue"
+            });
+        },
+
+        //Respond to any changes the user makes to the option method
+        _setOption: function ( key, value ) {
+            switch (key) {
+            case "someValue":
+                //this.options.someValue = doSomethingWith( value );
+                break;
+            default:
+                //this.options[ key ] = value;
+                break;
+            }
+
+            // For UI 1.8, _setOption must be manually invoked from
+            // the base widget
+            $.Widget.prototype._setOption.apply( this, arguments );
+            // For UI 1.9 the _super method can be used instead
+            //this._super( "_setOption", key, value );
+        }
+
+        //somewhere assetHtml would be used for templating, depending
+        // on your choice.
+    });
+})( jQuery, window, document );
+
+// If you are going to use the RequireJS optimizer to combine files
+// together, you can leave off the "ao.myWidget" argument to define:
+// define(["jquery", "text!templates/asset.html", "jquery-ui.custom.min"], …
+
+
+
+//usage: $("#myelem").foo( options );
+
+/* Some additional notes - delete this section before using the boilerplate.
+
+ We can also self-init this widget whenever a new page in jQuery Mobile is created. 
+ jQuery Mobile's "page" plugin dispatches a "create" event when a jQuery Mobile page 
+ (found via data-role=page attr) is first initialized.
+
+We can listen for that event (called "pagecreate" ) and run our plugin automatically
+ whenever a new page is created.
+
+$(document).bind("pagecreate", function (e) {
+    // In here, e.target refers to the page that was created
+    // (it's the target of the pagecreate event)
+    // So, we can simply find elements on this page that match a
+    // selector of our choosing, and call our plugin on them.
+    // Here's how we'd call our "foo" plugin on any element with a
+    // data-role attribute of "foo":
+    $(e.target).find("[data-role='foo']").foo(options);
+
+    // Or, better yet, let's write the selector accounting for the configurable
+    // data-attribute namespace
+    $(e.target).find(":jqmData(role='foo')").foo(options);
+});
+
+That's it. Now you can simply reference the script containing your widget and pagecreate 
+binding in a page running jQuery Mobile site, and it will automatically run like any 
+other jQM plugin.
+ */
