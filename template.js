4c8a3b5559dddba185558a371a308625d64b6f54
Rob Wood <flatulentdog@hotmail.com>
2016-01-28 06:39:04 -0500

    Must have had a quote replace got a little out of hand...

--- template.js
+++ template.js
@@ -12,11 +12,11 @@
 exports.description = 'Create a jQuery plugin stub/skeleton (using RequireJS with core dependencies and dev tasks setup) with [grunt-init][].';
 
 // Template-specific notes to be displayed before question prompts.
-exports.notes = '_Project name_ should not contain 'jquery' or 'js' and ' +
+exports.notes = '_Project name_ should not contain "jquery" or "js" and ' +
   'should be a unique ID not already in use at plugins.jquery.com. _Project ' +
   'title_ should be a human-readable title, and doesn\'t need to contain ' +
-  'the word 'jQuery', although it may. For example, a plugin titled 'Awesome ' +
-  'Plugin' might have the name 'awesome-plugin'.' +
+  'the word "jQuery", although it may. For example, a plugin titled "Awesome ' +
+  'Plugin" might have the name "awesome-plugin".' +
   '\n\n'+
   'For more information, please see the following documentation:' +
   '\n\n'+
